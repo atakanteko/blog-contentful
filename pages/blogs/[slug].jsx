b68fcb8f0ab58ac4decdcfae2cb9a7939d2f4532
Atakan Tekoğlu <atakantekoglu@gmail.com>
2023-01-31 14:08:33 +0300

    get slugs and create static pages for each slug

--- pages/blogs/[slug].jsx
+++ pages/blogs/[slug].jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { createClient } from "contentful";
 
-function BlogDetail() {
+function BlogDetail({ blogContent }) {
+  console.log(blogContent);
   return <div>BlogDetail</div>;
 }
 
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_KEY,
+});
+
+export async function getStaticPaths() {
+  const res = await client.getEntries({ content_type: "blog" });
+
+  const paths = res.items.map((item) => ({
+    params: { slug: item.fields.slug },
+  }));
+
+  // { fallback: false } means other routes should 404
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const res = await client.getEntries({
+    content_type: "blog",
+    "fields.slug": params.slug,
+  });
+  return {
+    props: { blogContent: res.items }, // will be passed to the page component as props
+  };
+}
 export default BlogDetail;
